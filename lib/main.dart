9aac604ac4929002acbf7055425c89b4b2198e6b
Yoftahe1 <yoftahemerkebu2@gmail.com>
2023-12-15 12:11:23 +0300

    socialsgram

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:socialsgram/helper/helper_function.dart';
+import 'package:socialsgram/pages/auth/login_page.dart';
+import 'package:socialsgram/pages/home_page.dart';
+import 'package:socialsgram/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        setState(() {
+          _isSignedIn = value;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Constants().primaryColor,
+          scaffoldBackgroundColor: Colors.white),
+      debugShowCheckedModeBanner: false,
+      home: _isSignedIn ? const HomePage() : const LoginPage(),
+    );
+  }
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  final primaryColor = const Color(0xFFee7b64);
+}
